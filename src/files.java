269dc2a7e3badd96b49795fefbd3e822ccdd7a7a
DmytroSobol <79514945+DmytroSobol@users.noreply.github.com>
2021-03-12 18:34:57 +0100

    Added cars and game

--- src/files.java
+++ src/files.java
@@ -100,6 +100,19 @@ public class files {
         catch (IOException e){
             System.out.println("Error");
         }
+        try {
+            File file = new File("./src/"+ (nazwa) + "/cars.txt");
+
+            if(!file.exists()){
+                file.createNewFile();
+            }
+            PrintWriter pw = new PrintWriter(file);
+            pw.println("Brak");
+            pw.close();
+        }
+        catch (IOException e){
+            System.out.println("Error");
+        }
     }
 
     //Sprawdza czy istnieje podany przez użytkownika login i hasło
